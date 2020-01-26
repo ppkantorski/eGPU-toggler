fe9e58956df6cf1c90dfaddc772d946505d478d1
Patrick Kantorski <ppkantorski@berkeley.edu>
2020-01-26 03:20:27 -0800

    Update eGPU-toggler.bat

--- eGPU-toggler.bat
+++ eGPU-toggler.bat
@@ -15,27 +15,29 @@ echo #========================================================================#
 echo # Programmer: Patrick Kantorski      ^| Date: 01/25/20                   #
 echo #========================================================================#
 
-start devmgmt.msc
+REM start devmgmt.msc
 
-echo ^> Starting PCIe controller toggler...
+"%mypath%"devcon.exe status "PCI\VEN_1002&DEV_66AF&SUBSYS_081E1002" > "%mypath%"eGPU-status.txt
+findstr /c:"12" "%mypath%eGPU-status.txt" && (echo ^> eGPU is not working properly!) || (goto :eof)
+
+echo ^> Starting eGPU PCIe controller toggler.
 timeout /t 1 >nul
-echo ^> 3
-timeout /t 1 >nul
-echo ^> 2
-timeout /t 1 >nul
-echo ^> 1
 
 
 for /l %%i in (1,1,500) do (
+
+  "%mypath%"devcon.exe status "PCI\VEN_1002&DEV_66AF&SUBSYS_081E1002" > "%mypath%"eGPU-status.txt
+  findstr /c:"12" "%mypath%eGPU-status.txt" && (echo ^> eGPU still not working yet...) || (goto :complete)
 
   "%mypath%"devcon.exe disable "PCI\VEN_8086&DEV_0D05"
   "%mypath%"devcon.exe enable "PCI\VEN_8086&DEV_0D05"
 
   timeout /t 1 >nul
-
-  "%mypath%"devcon.exe status "PCI\VEN_1002&DEV_66AF&SUBSYS_081E1002" > "%mypath%"eGPU-status.txt
-  findstr /c:"12" "%mypath%eGPU-status.txt" && (echo ^> egpu not connected...) || (echo ^> egpu is now connected! && goto :complete)
 )
 
+:notconnected
+echo ^> No issues with eGPU.
+goto :eof
+
 :complete
-pause
+echo ^> eGPU issues have been resolved!
